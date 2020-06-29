86581d3c0d915a44f05cd1f3efff2ddc8fe4fad1
onlyzyj <524350364@qq.com>
2020-06-29 22:05:08 +0800

    insert first version

---
+++ OneGit/src/main/java/zyj/User.java
@@ -0,0 +1,11 @@
+package zyj;
+
+/**
+ * @author onlyzyj
+ * @date 2020/6/29-21:45
+ */
+public class User {
+    public void show(){
+        System.out.println("show");
+    }
+}
---
+++ OneGit/src/test/java/zyj/UserTest.java
@@ -0,0 +1,12 @@
+package zyj;
+
+/**
+ * @author onlyzyj
+ * @date 2020/6/29-21:47
+ */
+public class UserTest {
+    public void showTest(){
+        User user = new User();
+        user.show();
+    }
+}
